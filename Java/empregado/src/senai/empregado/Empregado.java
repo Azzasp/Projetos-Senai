729fcbe85d7f3ab9c90ca2c983002ebc997913f8
Azzasp <p.erickbatista@gmail.com>
2022-07-24 10:00:36 -0300

    Creating var/getter and setter/constructor

--- Java/empregado/src/senai/empregado/Empregado.java
+++ Java/empregado/src/senai/empregado/Empregado.java
@@ -1,5 +1,54 @@
 package senai.empregado;
 
 public class Empregado {
+    private String nome;
+    private int anoNasc;
+    private int matricula;
+    private int anoContrato;
+    private boolean apto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoNasc() {
+        return anoNasc;
+    }
+
+    public void setAnoNasc(int anoNasc) {
+        this.anoNasc = anoNasc;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getAnoContrato() {
+        return anoContrato;
+    }
+
+    public void setAnoContrato(int anoContrato) {
+        this.anoContrato = anoContrato;
+    }
+
+    public boolean isApto() {
+        return apto;
+    }
+
+    public Empregado(String nome, int anoNasc, int matricula, int anoContrato) {
+        this.nome = nome;
+        this.anoNasc = anoNasc;
+        this.matricula = matricula;
+        this.anoContrato = anoContrato;
+    }
+
     
 }
