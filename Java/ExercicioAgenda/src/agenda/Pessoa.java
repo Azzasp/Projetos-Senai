ef0fee6158211596bbcc573c347500d4a0fde55a
Azzasp <p.erickbatista@gmail.com>
2022-07-24 11:12:46 -0300

    adding Pessoa attributes and methods

--- Java/ExercicioAgenda/src/agenda/Pessoa.java
+++ Java/ExercicioAgenda/src/agenda/Pessoa.java
@@ -1,6 +1,40 @@
 package agenda;
 
 public class Pessoa {
-    
+    private String nome;
+    private int idade;
+    private int altura;
+    private float cpf;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public float getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(float cpf) {
+        this.cpf = cpf;
+    }
 }
